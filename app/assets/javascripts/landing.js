20504cd5318f10e0e8825769aab6118f3e78f25e
Vipul A M <vipulnsward@gmail.com>
2014-05-10 12:45:29 +0530

    - Added temp favicon - Added Become speaker link - Use a fixed background

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
     var t = new Trianglify({
-            x_gradient: colorbrewer.Reds[Math.floor(Math.random() * 9) + 1],
-            y_gradient: colorbrewer.Reds[Math.floor(Math.random() * 9) + 1],
+//            x_gradient: ["#660000", "#800000", "#990000", "#B20000", "#CC0000", "#E60000", "FF0000", "#FF1919"] ,
+//            y_gradient: ["#660000", "#800000", "#990000", "#B20000", "#CC0000", "#E60000", "FF0000", "#FF1919"],
+            x_gradient: colorbrewer.Reds[9],
+            y_gradient: colorbrewer.Reds[1],
             noiseIntensity: 0.1,
-            cellpadding: 10,
+            cellpadding: 20,
             cellsize: 100}),
         header = ('.headingWrapper');
     var pattern = t.generate(document.body.clientWidth, document.body.clientHeight);
     $(header).attr('style', 'background-image: ' + pattern.dataUrl);
+
     $('.jcarousel').jcarousel({
         transitions: true,
         wrap: 'both'
